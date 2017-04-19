32c15a0758ab765afe1ad6b4f94987f1c27b5941
Patrick Chevalier <pachevalier@assystem.com>
2017-04-19 15:04:43 +0200

    [FORMATION] clean useless echo

--- scripts/init_ssh.sh
+++ scripts/init_ssh.sh
@@ -5,9 +5,7 @@ ssh-keygen -t dsa -f $HOME/.ssh/id_dsa
 cmd_grep=`ssh w553458 "cat ~/.ssh/authorized_keys2" | grep -c $DEV_NAME`
 if [ $cmd_grep == "0" ]
 then
-    echo "Export to the gateway computer."
     cat ~/.ssh/id_dsa.pub | ssh $GATEWAY "cat - >> .ssh/authorized_keys2"
 
-    echo "Add your key to ssh agent."
     ssh-add
 fi
--- scripts/manage_plt.sh
+++ scripts/manage_plt.sh
@@ -1,18 +1,14 @@
 if [[ $LD_ENV_PATH ]]
 then
-    echo "source $LD_ENV_PATH"
     source $LD_ENV_PATH
 else
-    echo "source env_ssh"
     source env_ssh
 fi
 
-echo "Do you need to generate ssh keys ? Yes(y/Y) or No(n/N) [N]"
 read rp
 
 if [[ "$rp" == "y" ]] || [[ "$rp" == "Y" ]]
 then
-    echo "Generate ssh keys."
     ./init_ssh.sh
 fi
 
@@ -62,32 +58,24 @@ maxport=45000
 port=0
 openports=$(netstat -lnt 2>&1 | grep tcp | grep -v tcp6 | awk '{print $4}' | awk -F: '{print $2}' | xargs echo -n)
 
-echo
-echo "Find a free port to open."
 for port in $(seq ${minport} ${maxport})
 do
   if ! [[ "${openports}" =~ "${port}" ]]
   then
 	find_openport=1
-	echo "Find port $port"
     break
   fi
 done
 
-echo
-echo "Create ssh tunnel"
 ssh -N -L localhost:${port}:${destination}:22 ${GATEWAY} &
 tunnelpid=$!
 
 # Give the tunnel some time
 sleep 1
 
-echo
-echo "Do you need to send data ? Yes(y/Y) or No(n/N) [N]"
 read rp
 if [[ "$rp" == "y" ]] || [[ "$rp" == "Y" ]]
 then
-  echo "Define one or several folder[s]/file[s] to tar : (use absolute path !)"
   read source
 
   if [[ $source == "" ]]
@@ -96,14 +84,11 @@ then
   else
       echo
       delivery=delivery$(date '+%Y-%m-%d_%H%M%S').tar
-      echo "Create archive $delivery of the $source source"
       tar -cvf $delivery $source
       res_tar=`echo $?`
 
       if [[ $res_tar == 0 ]]
       then
-	  echo
-	  echo "Transfer it to the $DEST_FOLDER folder of the corresponding platform (ip : $destination - user : $user)"
 	  scp -P ${port} -o StrictHostKeyChecking=no -o HostKeyAlias=${destination} $delivery $user@localhost:/tmp
       else
 	  echo "Fail to tar !"
@@ -112,24 +97,18 @@ then
   fi
 fi
 
-echo
-echo "Connect with ssh to $destination ? Yes(y/Y) or No(n/N) [N]"
 read rp
 if [[ "$rp" == "y" ]] || [[ "$rp" == "Y" ]]
 then
-    echo "Use exit command to quit."
     ssh -X -p ${port} -o StrictHostKeyChecking=no -o HostKeyAlias=${destination} $user@localhost
 else
     echo "If you keep ssh tunnel alive, you can connect to $destination with command :"
     echo "    ==> ssh -X -p ${port} -o StrictHostKeyChecking=no -o HostKeyAlias=${destination} $user@localhost"
 fi
 
-echo
-echo "Keep ssh tunnel alive ? Yes(y/Y) or No(n/N) [N]"
 read rp
 if [[ "$rp" == "n" ]] || [[ "$rp" == "N" ]] || [[ "$rp" == "" ]]
 then
-    echo "Kill ssh tunnel pid : $tunnelpid"
     kill $tunnelpid
 else
     echo "To kill ssh tunnel, use :"
